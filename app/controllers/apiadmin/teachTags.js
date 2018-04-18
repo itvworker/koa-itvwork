1fa39cdd90db27abf7f8d922bd77c06a3ffedae9
langjue <xieke76@qq.com>
2018-04-18 17:39:23 +0800

    0.0.33

--- app/controllers/apiadmin/teachTags.js
+++ app/controllers/apiadmin/teachTags.js
@@ -1,9 +1,28 @@
 const TeachTagsModel = require(path.join(webconfig.v1, 'teachTags.js'));
 class TeachTags {
-    async add(ctx, next) {
-        
+    async addOrUpdate() {
+      let post = this.ctx.request.body;
+      let data={
+        err_code:'201',
+        err_msg:'没有提交数据'
+      };
 
+      console.log();
+      if(post['data']&&post['data']['_id']){
+          data=await TeachTagsModel.update({_id:post['data']["_id"]},{title:post['data']['title']});
+      }
+      if(post['data']&&!post['data']['_id']){
+          data=await TeachTagsModel.add(post['data']);
+      }
+
+      this.ctx.body = data;
     }
+    async index(){
+      let post = this.ctx.request.body;
+      let data = await TeachTagsModel.find(post['data']);
+      this.ctx.body = data;
+    }
+
 
 
 }
